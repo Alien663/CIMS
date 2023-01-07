6a93ab9dcd5370f51f5ed2f24de534a74af394cd
11107063 <11107063@AN990154054>
2023-01-07 17:24:57 +0800

    modify vd_MachineVolume

--- DBShema/DBShema/dbo/Views/vd_MachineVolumes.sql
+++ DBShema/DBShema/dbo/Views/vd_MachineVolumes.sql
@@ -1,5 +1,6 @@
-﻿create view vd_MachineVolumes as
-select R.RID, R.MachineName, F.*
+﻿
+CREATE view [dbo].[vd_MachineVolumes] as
+select R.RID, R.MachineName, F.Driver, convert(varchar(10), Round(convert(decimal(5, 2) , F.Remaining * 100 ), 2)) + '%' as RemainingRate
 from RAWData R
 	cross apply openjson(R.Volume) with
 	(
