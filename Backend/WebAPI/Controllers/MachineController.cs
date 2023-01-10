cbdce959e747ffbabb6156dedf3f7f246a6a3fa7
Alien663 <rite2844@gmail.com>
2023-01-10 14:14:17 +0800

    feat:add Backend API

---
+++ Backend/WebAPI/Controllers/MachineController.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using WebAPI.Lib;
+using WebAPI.Models;
+using WebAPI.Filter;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [Login] // this will call Login Filter, you can also use in fron of api instead of controller
+    public class MachineController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMachineInformation()
+        {
+            List<MachineInformationModel> data = new List<MachineInformationModel>();
+            using(var db = new AppDb())
+            {
+                string sql = "select * from vd_MachineInformation";
+                data = db.Connection.Query<MachineInformationModel>(sql).ToList();
+            }
+            return Ok(data);
+        }
+
+        [HttpGet("user/{mid}")]
+        public IActionResult GetMachineUsers(int mid)
+        {
+            List<MachineUserModel> data = new List<MachineUserModel>();
+            using (var db = new AppDb())
+            {
+                string sql = @"select * from vd_MachineUsers where RID = @mid";
+                data = db.Connection.Query<MachineUserModel>(sql, new { mid }).ToList();
+            }
+            return Ok(data);
+        }
+
+        [HttpGet("volume/{mid}")]
+        public IActionResult GetMachineVolumes(int mid)
+        {
+            List<MachineVolumeModel> data = new List<MachineVolumeModel>();
+            using (var db = new AppDb())
+            {
+                string sql = @"select * from vd_MachineVolumes where RID = @mid";
+                data = db.Connection.Query<MachineVolumeModel>(sql, new { mid }).ToList();
+            }
+            return Ok(data);
+        }
+    }
+}
---
+++ Backend/WebAPI/Models/MachineModel.cs
@@ -0,0 +1,34 @@
+﻿namespace WebAPI.Models
+{
+    public class MachineInformationModel
+    {
+        public int RID { get; set; }
+        public string? FullName { get; set; }
+        public string? IPAddress { get; set; }
+        public string? OSType { get; set; }
+        public string? MachineType { get; set; }
+        public string? Manufacturer { get; set; }
+        public string? SystemFamily { get; set; }
+        public string? BIOSName { get; set; }
+        public string? BIOSVersion { get; set; }
+    }
+
+    public class MachineUserModel
+    {
+        public int RID { get; set; }
+        public string? MachineName { get; set; }
+        public string? Group { get; set; }
+        public string? Account { get; set; }
+        public Boolean isDomain { get; set; }
+        public Boolean isActive { get; set; }
+    }
+    public class MachineVolumeModel
+    {
+        public int RID { get; set; }
+        public string? MachineName { get; set; }
+        public string? Driver { get; set; }
+        public string? DriveType { get; set; }
+        public string? RemainingRate { get; set; }
+        public string? RemainintSize { get; set; }
+    }
+}
